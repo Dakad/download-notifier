ce29ad74a1f2ef834f231fdd44051f7e68832c2c
Dakad <>
2018-02-03 02:46:53 +0100

    Clean notifications.js

--- js/utils.js
+++ js/utils.js
@@ -5,7 +5,7 @@
      * @param {string} id ID of the element WITHOUT #
      * @returns {HTMLElement}
      */
-    const _$ = id => doc.getElementById(id);
+    const _$ = id => document.getElementById(id);
 
     /**
      * Get the corresponding message to a specified key
@@ -13,3 +13,29 @@
      * @return {string}
      */
     const _i18n = key => chrome.i18n.getMessage(key);
+
+
+    /**
+     * @typedef CurrentOS
+     * @property {Windows} string "windows"
+     * @property {Mac} string "Mac"
+     * @property {Other} string "Other"
+     */
+    /**
+     * Get the current navigator OS
+     * @return {CurrentOS} The corresponding OS name
+     */
+    const _getOS = () => {
+        const _ua = navigator.userAgent.toLowerCase();
+        let _os;
+
+        if (_ua.length) {
+            if (_ua.includes('windows')) {
+                _os = 'windows';
+            } else {
+                _os = (_ua.includes('mac') ? 'mac' : 'other');
+            }
+        }
+
+        return _os;
+    };
